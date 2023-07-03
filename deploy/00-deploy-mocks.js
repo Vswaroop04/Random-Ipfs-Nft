8bd77d823aaf1dc3ee7037a30b9b4a8ab86dc92f
Vishnu Swaroop <97403550+Vswaroop04@users.noreply.github.com>
2023-07-03 22:15:11 +0530

    Update 00-deploy-mocks.js

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -16,13 +16,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             log: true,
             args: [BASE_FEE, GAS_PRICE_LINK],
         })
+        // await deploy("MockV3Aggregator", {
+        //     from: deployer,
+        //     log: true,
+        //     args: [DECIMALS, INITIAL_PRICE],
+        // })
         await deploy("MockV3Aggregator", {
             from: deployer,
             log: true,
             args: [DECIMALS, INITIAL_PRICE],
         })
 
-        log("Mocks Deployed!")
+        log("Mocks Deployed!!")
         log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         log("You are deploying to a local network, you'll need a local network running to interact")
         log(
